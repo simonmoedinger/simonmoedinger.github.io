724aff41a619381b70fc8fa8c5d50732ec2d3324
simon <smoeding@stu.hs-heilbronn.de>
2023-02-23 12:24:06 +0100

    update

--- docs/src/.vuepress/config.ts
+++ docs/src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/https://simonmoedinger.github.io/",
+  base: "/",
 
   locales: {
     "/": {
